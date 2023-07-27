2d72fb88f389fbb27201dc58cb982c3b8982fba9
Luke Freeland <luke.freeland@ndsu.edu>
2023-07-26 21:56:27 -0500

    Added in arguments to allow user to specify password length. Still a WIP with some errors.

--- password.py
+++ password.py
@@ -1,14 +1,35 @@
 import string
 import random
+import argparse
 
-special_char = "!@#$%^&*()_-"
+class Generate:
+    parser = argparse.ArgumentParser(description='A program to generate a random password.')
+    parser.add_argument("-l", "--length",
+                        help='Please enter a number to determine the length of your password. The default lengh is 10.',
+                        type=int)
+    args = parser.parse_args()
 
-def generate_password(length):
-    chars = string.ascii_letters + string.digits + special_char
-    password = ''.join(random.choice(chars) for _ in range(length))
-    return password
+    def __init__(self):
+        self.special_char = "!@#$%^&*()_-"
+        self.password_length = 0
+
+    def generate_password(self, length):
+        chars = string.ascii_letters + string.digits + self.special_char
+        password = ''.join(random.choice(chars) for _ in range(length))
+        return password
+
+    def main(self):
+        if(self.args.length == None):
+            self.password_length = 10
+        else:
+            self.pasword_length = self.args.length
+
+        print(self.args.length)
+        
+        rand_password = self.generate_password(self.password_length)
+        print(f"Generated Password: {rand_password}")
 
 if __name__ == "__main__":
-    password_length = 10
-    rand_password = generate_password(password_length)
-    print(f"Generated Password: {rand_password}")
+    generate = Generate()
+    generate.main()
+        
